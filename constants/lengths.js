6a99a9f5f67484ad5d740b9bdd87801700b37460
Cynthia Tedesco <cynthia.tedesco@gmail.com>
2020-05-28 09:20:49 +0200

    Add constants

---
+++ constants/lengths.js
@@ -0,0 +1,11 @@
+export default [
+  "100 minutes",
+  "110 minutes",
+  "120 minutes",
+  "130 minutes",
+  "140 minutes",
+  "150 minutes",
+  "160 minutes",
+  "170 minutes",
+  "180 minutes"
+];
---
+++ constants/wordCounts.js
@@ -0,0 +1,9 @@
+export default [
+  "40 words/min",
+  "50 words/min",
+  "60 words/min",
+  "70 words/min",
+  "80 words/min",
+  "90 words/min",
+  "100 words/min",
+];
